1c595b6913ab0248d668c84899c901da2874f76b
Marius Nettum <nettum@gmail.com>
2018-11-12 18:33:36 +0100

    nginx, php, mysql

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,16 @@ add-apt-repository ppa:snwh/ppa
 apt-get update
 apt install adapta-gtk-theme paper-icon-theme
 
-# nginx, php
+# nginx, php, mysql
 apt install nginx
 apt install php-fpm
+apt install php-xml php-curl php-mbstring php-zip
+apt install mysql-server
+mysql_secure_installation
+
+# composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
