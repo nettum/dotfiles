34622543cc0749d2ca12579a95a8c655c8147afb
Marius Nettum <nettum@gmail.com>
2018-11-12 17:09:10 +0100

    Add first version of bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+# install software
+snap install spotify
+snap install slack
+snap install vscode
+apt install terminator
+
+# setup gnome tweak tools adapta wm and icons
+apt install gnome-tweak-tool
+apt-get install chrome-gnome-shell
+add-apt-repository ppa:tista/adapta
+add-apt-repository ppa:snwh/ppa
+apt-get update
+apt install adapta-gtk-theme paper-icon-theme
+
+# nginx, php
+apt install nginx
+apt install php-fpm
