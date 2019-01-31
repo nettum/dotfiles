b8fe2b829b825c9812a137f302cb139f596da497
Marius Nettum <nettum@gmail.com>
2019-01-31 18:11:46 +0100

    Add kazam

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ sudo apt install -y handbrake
 sudo apt install -y virtualbox
 sudo apt install -y virtualbox-ext-pack
 sudo apt install -y flameshot
+sudo apt install -y kazam
 sudo apt install -y gnome-sushi
 sudo apt install -y dconf-editor
 sudo apt install -y terminator
